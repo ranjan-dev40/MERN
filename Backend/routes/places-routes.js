2438a607d70d86f1279287e9e0daf7711befab8e
ranjan-dev40 <roy984258@gmail.com>
2023-06-29 22:45:39 +0530

    revert back

--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("places/user/:uid", placesControllers.getPlacesByUserId);
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
 
 router.use(checkAuth);
