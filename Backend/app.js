8d8c9bb3d59a3bc1d8755f5c029e8228a6179dac
ranjan-dev40 <roy984258@gmail.com>
2023-07-05 20:42:59 +0530

    changing images url

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/uploads/images', express.static(path.join('uploads','images')));
+// app.use('/uploads/images', express.static(path.join('uploads','images')));
+app.use('/images', express.static(path.join('uploads','images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
